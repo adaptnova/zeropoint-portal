d8605bce19d65240ffbdde537dc1906fa5afd684
ADAPT-Chse <chase@levelup2x.com>
2025-09-18 08:34:55 +0000

    feat: Scaffold repository and initialize with spec-kit

--- images/vllm/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 1.0.0 - 2025-09-18
-
-- Initial release.
--- images/vllm/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Default to running the vllm api server if no command is provided
-if [ "$1" = "" ]; then
-    exec python -m vllm.entrypoints.api_server
-else
-    exec "$@"
-fi
